f4e37ea3c70c5317f0ae67cf5ad56324f28b0cc8
Gustavo-B-Morales-S <protected_gustavo_borges@outlook.com>
2024-08-06 11:28:38 -0300

    Added: Models & Users Model

---
+++ speedapi/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import String, func
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+metadata_registry = registry()
+
+
+@metadata_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    id: Mapped[intpk] = mapped_column(init=False)
+
+    username: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        init=False,
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False,
+        server_default=func.now(),
+    )
