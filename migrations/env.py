871b0504e24c7ff29fecc5cadfa4302d6567cf64
Gustavo-B-Morales-S <protected_gustavo_borges@outlook.com>
2024-08-08 10:18:22 -0300

    Apply: Code formmating and linting fixes

--- migrations/env.py
+++ migrations/env.py
@@ -40,12 +40,12 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option('sqlalchemy.url')
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        dialect_opts={'paramstyle': 'named'},
     )
 
     with context.begin_transaction():
@@ -61,14 +61,12 @@ def run_migrations_online() -> None:
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+        prefix='sqlalchemy.',
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- speedapi/app.py
+++ speedapi/app.py
@@ -1,11 +1,11 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
-from speedapi.database import get_session
-from speedapi.models import User
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from speedapi.database import get_session
+from speedapi.models import User
 from speedapi.schemas import Message, UserPrivate, UserPublic, UsersList
 
 app = FastAPI(debug=True)
@@ -21,18 +21,10 @@ def read_root():
     return {'message': 'Welcome to SpeedAPI!'}
 
 
-@app.post('/users/',
-          status_code=HTTPStatus.CREATED,
-          response_model=UserPublic)
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserPrivate, session: Session = Depends(get_session)):
     verify_user_in_database = session.scalar(
-
-
-        select(User).filter_by(
-            username=user.username,
-            email=user.email
-
-        )
+        select(User).filter_by(username=user.username, email=user.email)
     )
 
     if verify_user_in_database:
